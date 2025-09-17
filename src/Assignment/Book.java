9d8b4ebc2d7e56af201f8050983169dab942e213
anph1000 <anph1000@stud.ek.dk>
2025-09-17 09:38:59 +0200

    bookclass1

--- src/Assignment/Book.java
+++ src/Assignment/Book.java
@@ -1,4 +1,23 @@
 package Assignment;
 
-public class Book {
+//Subclass: Book
+public class Book extends Items {
+
+    public Book(String name) {
+        super(title);
+        super(type);
+    }
+
+    //Her overrider vi type, da det er specefikt en bog
+    @overide
+    public String type{
+        return "book";
+    }
+
+    @Override
+    public String toString() {
+        return "book" +
+                ": " + title;
+    }
 }
+
