8c4eba0e1f6668d6fd434ebeb54beaa03da6ae2a
Nikolaj Esberg <nikolajesberg@gmail.com>
2025-09-17 12:06:08 +0200

    Final v10

--- src/Assignment/LibraryTest.java
+++ src/Assignment/LibraryTest.java
@@ -1,49 +1,65 @@
 package Assignment;
 
+// Importerer Scanner til at læse input
 import java.util.Scanner;
 
 public class LibraryTest {
     public static void main(String[] args) {
+        // Opretter en Scanner til at læse input fra brugeren
         Scanner scanner = new Scanner(System.in);
 
+        // Skriver en velkomstbesked til kunden
         System.out.println("Velkommen til vores bibliotek!");
 
+        // Kunden indtaste, hvor mange items der skal registreres
         System.out.print("Hvor mange Items vil du gerne registrere? ");
+        // Læser antal items fra kunden
         int antal = scanner.nextInt();
-        scanner.nextLine(); // Rydder scanneren
+        scanner.nextLine();
 
-        // Oprettelse af array
+        // Opretter et array af Items
         Items[] items = new Items[antal];
 
+        // Starter en for løkke til at registrere hvert enkelt item
         for (int i = 0; i < antal; i++) {
+            // Informerer kunden om, hvilket item nummer der skal indtastes
             System.out.println("\nRegistrer Item #" + (i + 1));
 
+            // Spørger kunden hvilken Iten (enten 'Book' eller 'Video')
             System.out.print("Type (Book, Video): ");
+            // Læser item som en streng og laver til små bogstaver
             String type = scanner.nextLine().toLowerCase();
 
+            // Spørger om titlen på item
             System.out.print("Title: ");
+            // Læser titlen som en streng
             String title = scanner.nextLine();
 
+            // Afhængig af kundens input oprettes det korrekte obje kt
             if (type.equals("book")) {
+                // Opretter et Book-objekt med den indtastede titel og gemmer i arrayet
                 items[i] = new Book(title);
             } else if (type.equals("video")) {
+                // Opretter et Video-objekt med den indtastede titel og gemmer i arrayet
                 items[i] = new Video(title);
             } else {
-                System.out.println("Ukendt Item - oprettes som generisk Item");
+                // Hvis typen ikke er genkendt, gives besked til brugeren
+                System.out.println("Ukendt Item");
                 items[i] = new Items(title);
             }
         }
 
-        // Print overskrift
+        // Udskriver overskriften for listen af registrerede items
         System.out.println("\nListe af låne Items");
-        System.out.println("__________________\n");
-        System.out.println("\nID\tType\tTitle");
+        System.out.println("--------------------\n");
+        System.out.println("ID\tType\tTitle");
 
-        // Print listen over items med ID, type og titel
+        // Løkke til at udskrive hvert item med ID
         for (int i = 0; i < items.length; i++) {
-            System.out.println((i + 1) + " " + items[i].getType() + " " + items[i].title);
+            System.out.println((i + 1) + "\t" + items[i].getType() + "\t" + items[i].title);
         }
 
+        // Lukker Scanner
         scanner.close();
     }
 }
