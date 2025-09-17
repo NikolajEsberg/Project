e5cf80aa9fd109d7b1fff3ab25374771143091f6
Cheriks <cher002@stud.ed.dk>
2025-09-17 09:53:01 +0200

    blaf

--- src/Assignment/Video.java
+++ src/Assignment/Video.java
@@ -1,4 +1,22 @@
 package Assignment;
 
-public class Video {
+//Subclass: Video
+public class Video extends Items {
+
+    public Video(String name) {
+        super(title);
+        super(type);
+    }
+
+    //Her overrider vi type, da det er specifikt en video
+    @Override
+    public String type{
+        return "video";
+    }
+
+    @Override
+    public String toString() {
+        return "video" +
+                ": " + title;
+    }
 }
